9ddc714b0acaf4dede76090fb5910aa101d04839
wiarda <wiarda@gmail.com>
2018-03-01 00:37:51 +0800

    sass dependencies for bootstrap import

--- webpack.config.js
+++ webpack.config.js
@@ -18,12 +18,54 @@ module.exports = {
       //css for loading in bootstrap's min.css
       ,{ test:/\.css$/
         ,use:['style-loader', 'css-loader']}
-    ]
-  }
+
+      //scss for bootstrap
+      ,{ test: /\.(scss)$/
+        ,use: [{loader: 'style-loader', // inject CSS to page
+                }
+              ,{loader: 'css-loader', // translates CSS into CommonJS modules
+                }
+              ,{loader: 'postcss-loader', // Run post css actions
+                options: {
+                  plugins: function () { // post css plugins, can be exported to postcss.config.js
+                    return [
+                      require('precss'),
+                      require('autoprefixer')
+                    ];
+                  }
+                }
+              }
+              ,{loader: 'sass-loader' // compiles Sass to CSS
+              }]
+      }
+
+    ] // end rules array
+  } //end module object
   ,output: {
-     filename: 'index.jsx'
+     filename: 'index.js'
     ,path: __dirname + '/build'
     }
   ,plugins: [HTMLWebpackPluginConfig]
 
 }
+
+
+//
+// ,{ test: /\.(scss)$/
+//   ,use: [{loader: 'style-loader', // inject CSS to page
+//           }
+//         ,{loader: 'css-loader', // translates CSS into CommonJS modules
+//           }
+//         ,{loader: 'postcss-loader', // Run post css actions
+//           options: {
+//             plugins: function () { // post css plugins, can be exported to postcss.config.js
+//               return [
+//                 require('precss'),
+//                 require('autoprefixer')
+//               ];
+//             }
+//           }
+//         }
+//         ,{loader: 'sass-loader' // compiles Sass to CSS
+//         }]
+// }
